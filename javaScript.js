da73842a2905a3911764b65071e11f8db5f49a3a
Moamal6161 <100538652+Moamal6161@users.noreply.github.com>
2023-08-25 17:50:41 +0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,25 @@
+console.log("hello my name is muamal i from iraq")
+function darkmode(){
+    document.getElementById("header").style.border = "3px solid white";
+    document.getElementById("body").style.background = "#272727";
+    document.getElementById("lightmode").style.border = "3px solid white";
+    document.getElementById("lightmode").style.color = "white";
+    document.getElementById("lightmode").style.background = "#272727";
+    document.getElementById("darkmode").style.border = "3px solid white";
+    document.getElementById("darkmode").style.color = "white";
+    document.getElementById("darkmode").style.background = "#272727";
+    document.getElementById("article0").style.border = "3px solid white";
+    document.getElementById("article1").style.border = "3px solid white";
+}
+function lightmode(){
+    document.getElementById("header").style.border = "3px solid black";
+    document.getElementById("body").style.background = "white";
+    document.getElementById("lightmode").style.border = "3px solid black";
+    document.getElementById("lightmode").style.color = "black";
+    document.getElementById("lightmode").style.background = "white";
+    document.getElementById("darkmode").style.border = "3px solid black";
+    document.getElementById("darkmode").style.color = "black";
+    document.getElementById("darkmode").style.background = "white";
+    document.getElementById("article0").style.border = "3px solid black";
+    document.getElementById("article1").style.border = "3px solid black";
+}
---
+++ wew.py
@@ -0,0 +1,8 @@
+import sys
+import colorama
+from colorama import Fore 
+import time
+import webbrowser
+idk = input("link : ")
+hello = get(idk)
+hello.open()
